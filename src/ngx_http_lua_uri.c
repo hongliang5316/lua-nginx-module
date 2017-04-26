7aad3fa918922b396d1e13d6841827bc3c1eb131
hongliang <hongliang5316@gmail.com>
2017-04-26 22:15:25 +0800

    feature: add FFI function `ngx_http_lua_ffi_req_get_ext`

--- src/ngx_http_lua_uri.c
+++ src/ngx_http_lua_uri.c
@@ -107,4 +107,20 @@ ngx_http_lua_ngx_req_set_uri(lua_State *L)
     return 0;
 }
 
+
+#ifndef NGX_LUA_NO_FFI_API
+int
+ngx_http_lua_ffi_req_get_uri_ext(ngx_http_request_t *r, char **buf, size_t *len)
+{
+    if (r->connection->fd == (ngx_socket_t) -1) {
+        return NGX_HTTP_LUA_FFI_BAD_CONTEXT;
+    }
+
+    *len = ngx_min(r->exten.len, *len);
+    *buf = (char *) r->exten.data;
+    return NGX_OK;
+}
+#endif /* NGX_LUA_NO_FFI_API */
+
+
 /* vi:set ft=c ts=4 sw=4 et fdm=marker: */
